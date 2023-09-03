169c6e6fe515f3db71be7d57beb97127d11ab616
Okiki Areo <okikiareo@gmail.com>
2023-09-03 20:41:19 +0100

    mode done

--- scripts/mode.js
+++ scripts/mode.js
@@ -16,3 +16,121 @@ function darkTheme() {
   document.body.classList.add('dark');
   localStorage.setItem('calc-mode', 'dark');
 }
+
+function paleTheme() {
+    button.src = 'images/icon_sun.svg';
+    document.body.classList.remove('dark');
+    localStorage.setItem('calc-mode', null);
+  }
+  
+  window
+  .addEventListener('DOMContentLoaded', () => {
+    mode == 'dark' ? darkTheme() : paleTheme();
+  });
+  
+  button
+  .addEventListener('click', () => pickTheme());
+  function paleTheme() {
+    button.src = 'images/icon_sun.svg';
+    document.body.classList.remove('dark');
+    localStorage.setItem('calc-mode', null);
+  }
+  
+  window
+  .addEventListener('DOMContentLoaded', () => {
+    mode == 'dark' ? darkTheme() : paleTheme();
+  });
+  
+  button
+  .addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
