3a8cb21f94423324d758daf43865a1ad459eba3d
Okiki Areo <okikiareo@gmail.com>
2023-09-03 20:37:46 +0100

    initial js

---
+++ scripts/mode.js
@@ -0,0 +1,18 @@
+let mode = localStorage.getItem('calc-mode');
+let button = document.querySelector('[data-toggle]')
+
+function pickTheme() {
+  if (mode == 'dark') {
+    paleTheme();
+    mode = localStorage.getItem('calc-mode');
+  } else {
+    darkTheme();
+    mode = localStorage.getItem('calc-mode');
+  }
+}
+
+function darkTheme() {
+  button.src = 'images/icon_moon.svg';
+  document.body.classList.add('dark');
+  localStorage.setItem('calc-mode', 'dark');
+}
