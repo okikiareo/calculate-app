06e0d9c0cdbf5df3c967bad3ac65c197cd24b3bb
Okiki Areo <okikiareo@gmail.com>
2023-09-04 02:07:10 +0100

    main

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,70 @@ function setInnerText(e) {
   let { el, text } = e;
   el.innerText += text;
 }
+
+class App {
+    appendNumber(e) {
+      let key = e.innerText;
+      
+      if (result != '') {
+        result = '';
+        operands = [];
+        keys = '';
+      }
+      
+      if (key != '0') {
+        if (keys == '0') keys = '';
+      }
+      
+      if (key == '.') {
+        if (keys.includes('.')) return;
+        if (keys == '') keys = '0';
+        if (keys == '-') keys = keys + '0';
+      }
+      
+      if (key == '0') {
+        if (keys == '0') return;
+      }
+      
+      keys = keys + key;
+      operands[index] = keys;
+      app.revealButton(keys);
+    }
+    
+    appendSymbol(e) {
+      let key = e.innerText;
+      
+      if (operands.length == 2) {
+        app.returnResult();
+        app.cachesResult();
+      }
+      
+      if (keys == '' && index == 0) {
+        if (key != '-') return;
+        keys = keys + key;
+        app.revealButton(keys);
+      }
+      
+      if (keys == '' && index == 1) {
+        if (key == '-' && operator == '*' || key == '-' && operator == '/') {
+          keys = keys + key;
+          app.revealButton(keys);
+        } else {
+          operator = '';
+          index = 0;
+          keys = operands[index];
+        }
+      }
+      
+      if (operands[0] != undefined) {
+        if (index != 0) return;
+        if (result != '') result = '';
+        keys = keys + key;
+        app.revealButton(keys);
+        operator = key;
+        index = 1;
+        keys = '';
+      }
+    }
+    
+  
